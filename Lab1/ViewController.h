0b0e7795ab0df48750b78da269d8fa712d2aed1a
LuckyDjuk <alf.hammerseth@gmail.com>
2014-08-07 13:34:48 +1000

    Initial Commit

---
+++ Lab1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Lab1
+//
+//  Created by Alf Magnus Kittang Hammerseth on 07.08.14.
+//  Copyright (c) 2014 Alf Magnus Kittang Hammerseth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Lab1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Lab1
+//
+//  Created by Alf Magnus Kittang Hammerseth on 07.08.14.
+//  Copyright (c) 2014 Alf Magnus Kittang Hammerseth. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
