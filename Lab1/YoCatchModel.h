4a2bb68249fa0e2ae68f8ffa7b8276b40a8cf8af
Alf Hammerseth <alf.hammerseth@gmail.com>
2014-08-11 14:49:01 +1000

    Cleanup

--- Lab1/YoCatchModel.h
+++ Lab1/YoCatchModel.h
@@ -10,14 +10,5 @@
 
 @interface YoCatchModel : NSObject
 
- /* Name of the Yo user. Currently this is local */
- @property (nonatomic, strong) NSString* username;
- /* History of the messages sent with Yo */
- @property (nonatomic, strong, readonly) NSMutableArray* historyArray;
- /* Class method for getting the default Yo message Question 6 answer here. */
- + (NSString*) defaultYoMessage;
-
- // Instance method for adding data into the history array
- // Your answer to q7 here
 
 @end
